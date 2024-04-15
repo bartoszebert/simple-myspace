bff83e80e8c2bf24e8bd2464649621fd18c3744e
Bartosz Ebert <bartoszebert@gmail.com>
2024-04-15 09:11:56 +0200

    change logo for icon

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -8,7 +8,7 @@ export default function NavMenu(): JSX.Element {
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <Image src="/logo.svg" width={216} height={30} alt="BartSpace " />
+        <Image src="/logo.svg" width={80} height={35} alt="BartSpace " />
       </Link>
       <ul className={styles.links}>
         <li>
